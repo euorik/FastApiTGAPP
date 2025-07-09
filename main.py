3f3c81d8ea1880e97243ee6f5b099ea14ef62bf6
Evgeni Rakintsev <euorik@yandex.com>
2025-07-09 17:22:39 +0300

    Изменение ip адресса

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ async def complete_task(task: CompleteTask):
     return {'status': 'ok'}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="172.17.0.2", port=8000)
